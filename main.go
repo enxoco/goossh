7006491ae24e5be4d83ad4977256228c8501447f
Mike Conrad <mike@enxo.co>
2024-06-23 13:25:49 -0400

    Removed prometheus and keats

--- main.go
+++ main.go
@@ -1,31 +1,21 @@
 package main
 
 import (
-	"encoding/base64"
 	"log"
-	"math/rand"
 	"net"
-	"net/http"
 	"time"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
-	keats      = "O soft embalmer of the still midnight! Shutting with careful fingers and benign Our gloom-pleased eyes, embower’d from the light, Enshaded in forgetfulness divine; O soothest Sleep! if so it please thee, close, In midst of this thine hymn, my willing eyes, Or wait the amen, ere thy poppy throws Around my bed its lulling charities."
-	lineLength = 64
 	delay      = 10 * time.Second
-	maxConns   = 4
 )
 
 func main() {
+	// Create a serve on port 2222
 	ln, err := net.Listen("tcp", ":2222")
 	if err != nil {
 		log.Panic("Could not create listener:", err)
 	}
-
-	http.Handle("/metrics", promhttp.Handler())
-	go http.ListenAndServe(":80", nil)
 
 	var conns int
 	connDone := make(chan bool)
@@ -43,11 +33,6 @@ func main() {
 		if err != nil {
 			log.Panic("could not accept connection: ", err)
 		}
-		if conns >= maxConns {
-			log.Println("Connection attempted but hit max, closing connection")
-			conn.Close()
-			continue
-		}
 
 		conns++
 		log.Printf("Connection from %s - Number of connections is now %d", conn.RemoteAddr().String(), conns)
@@ -60,23 +45,16 @@ func handleConnection(conn net.Conn, done chan<- bool) {
 	defer timeTaken(time.Now(), conn.RemoteAddr().String())
 
 	for {
-		ln := generateLine(keats)
-		_, err := conn.Write(ln)
+		// This is the banner we want to send to the client
+		_, err := conn.Write([]byte("connecting..." + "\r\n"))
+
 		if err != nil {
 			done <- true
 			return
 		}
+		// Wait for this amount of time before sending the next banner.
 		time.Sleep(delay)
 	}
-}
-
-func generateLine(text string) []byte {
-	length := len(text)
-	index := rand.Intn(length - lineLength - 1)
-	runes := []rune(keats)
-	msg := string(runes[index:index+lineLength]) + "\n\n\n"
-	encoded := base64.StdEncoding.EncodeToString([]byte(msg))
-	return []byte(encoded)
 }
 
 func timeTaken(t time.Time, client string) {
